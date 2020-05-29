c0f67add8c8fcef3e9df2ddf9d636ee0ba7fb3a6
kishoretaru <tarunkishore017@gmail.com>
2020-05-29 18:10:35 +0530

    Context Api

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Fragment } from 'react';
+import Provider from "./Provider";
+import Context from "./context";
+
+const Agents = () => {
+  return <AgentOne />
+}
+
+const AgentOne = () => {
+  return <AgentTwo />
+}
+
+const AgentTwo = () => {
+  return <AgentBond />
+}
+
+const AgentBond = () => {
+  return(
+    <Context.Consumer>
+    {
+      (context) => (
+        <Fragment>
+        <h3>Agent Information</h3>
+        <p>Missiion Name : {context.data.mname}</p>
+        <h2>Mission state:{context.data.accept}</h2>
+        <button onClick={context.isMissionAccepted}>Click to change</button>
+        </Fragment>
+      )
+    }
+    </Context.Consumer>
+  );
+}
+
+
+
+const App = () => {
+   return(
+     <div>
+     <h1>Context Api</h1>
+     <Provider>
+     <Agents />
+     </Provider>
+     </div>
+   );
+};
+
+export default App;
